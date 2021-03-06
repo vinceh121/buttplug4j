119adc3573130c2c480a69febdc8f5ab215d0cbf
vinceh121 <contact@vinceh121.me>
2021-03-06 15:44:43 +0100

    Slightly better exceptions

--- buttplug.core/src/main/java/org/metafetish/buttplug/core/ButtplugJsonMessageParser.java
+++ buttplug.core/src/main/java/org/metafetish/buttplug/core/ButtplugJsonMessageParser.java
@@ -1,10 +1,10 @@
 package org.metafetish.buttplug.core;
 
-import java.io.IOException;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectMapper.DefaultTypeResolverBuilder;
@@ -19,22 +19,22 @@ public class ButtplugJsonMessageParser {
 		this.mapper = new ObjectMapper();
 		TypeResolverBuilder<?> typer = DefaultTypeResolverBuilder.construct(DefaultTyping.JAVA_LANG_OBJECT,
 				this.mapper.getPolymorphicTypeValidator());
-		// reassignment here because documentation says "Resulting builder instance
-		// (usually this builder, but not necessarily)"
+		// reassignment here because documentation says "@returns Resulting builder
+		// instance (usually this builder, but not necessarily)"
 		typer = typer.init(JsonTypeInfo.Id.NAME, null);
 		typer = typer.inclusion(As.WRAPPER_OBJECT);
 		this.mapper.setDefaultTyping(typer);
 	}
 
-	public List<ButtplugMessage> parseJson(final String json) throws IOException {
+	public List<ButtplugMessage> parseJson(final String json) throws JsonProcessingException {
 		return this.mapper.readValue(json, new TypeReference<List<ButtplugMessage>>() {});
 	}
 
-	public String formatJson(final List<ButtplugMessage> msgs) throws IOException {
+	public String formatJson(final List<ButtplugMessage> msgs) throws JsonProcessingException {
 		return this.mapper.writeValueAsString(msgs);
 	}
 
-	public String formatJson(final ButtplugMessage msgs) throws IOException {
+	public String formatJson(final ButtplugMessage msgs) throws JsonProcessingException {
 		return this.mapper.writeValueAsString(new ButtplugMessage[] { msgs });
 	}
 
